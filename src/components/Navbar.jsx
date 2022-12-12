59b885fa6e932d931af02566f3c04ac25c90a8b0
Nishant Kumar Singh <nishant220902@gmail.com>
2022-12-13 00:13:46 +0530

    changed font size and text color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,13 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <nav className="navbar navbar-expand-lg bg-black navbar-dark">
           <div className="container-fluid">
-            <Link className="navbar-brand fs-2" to="/general">
+            <Link
+              className="navbar-brand"
+              to="/general"
+              style={{ fontSize: "3rem" }}
+            >
               Newzify
             </Link>
             <button
@@ -18,46 +22,112 @@ export class Navbar extends Component {
               aria-controls="navbarSupportedContent"
               aria-expanded="false"
               aria-label="Toggle navigation"
+              style={{
+                color: "white",
+                border: "1px solid white",
+                borderRadius: "0.5rem",
+              }}
             >
-              <span className="navbar-toggler-icon"></span>
+              <span
+                className="navbar-toggler-icon"
+                style={{
+                  color: "white",
+                }}
+              ></span>
             </button>
             <div
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item items-list">
-                  <Link className="nav-link" to="/General">
+                <li className="nav-item">
+                  <a
+                    className="nav-link mx-md-2 text-center"
+                    href="/General"
+                    style={{
+                      color: "white",
+                      border: "2px solid white",
+                      borderRadius: "0.5rem",
+                    }}
+                  >
                     General
-                  </Link>
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Business">
+                  <a
+                    className="nav-link mx-md-2 text-center"
+                    href="/Business"
+                    style={{
+                      color: "white",
+                      border: "2px solid white",
+                      borderRadius: "0.5rem",
+                    }}
+                  >
                     Business
-                  </Link>
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Entertainment">
+                  <Link
+                    className="nav-link mx-md-2 text-center"
+                    to="/Entertainment"
+                    style={{
+                      color: "white",
+                      border: "2px solid white",
+                      borderRadius: "0.5rem",
+                    }}
+                  >
                     Entertainment
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Health">
+                  <Link
+                    className="nav-link mx-md-2 text-center"
+                    to="/Health"
+                    style={{
+                      color: "white",
+                      border: "2px solid white",
+                      borderRadius: "0.5rem",
+                    }}
+                  >
                     Health
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Science">
+                  <Link
+                    className="nav-link mx-md-2 text-center"
+                    to="/Science"
+                    style={{
+                      color: "white",
+                      border: "2px solid white",
+                      borderRadius: "0.5rem",
+                    }}
+                  >
                     Science
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Sports">
+                  <Link
+                    className="nav-link mx-md-2 text-center"
+                    to="/Sports"
+                    style={{
+                      color: "white",
+                      border: "2px solid white",
+                      borderRadius: "0.5rem",
+                    }}
+                  >
                     Sports
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Technology">
+                  <Link
+                    className="nav-link mx-md-2 text-center"
+                    to="/Technology"
+                    style={{
+                      color: "white",
+                      border: "2px solid white",
+                      borderRadius: "0.5rem",
+                    }}
+                  >
                     Technology
                   </Link>
                 </li>
