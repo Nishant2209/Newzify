8f28ed3414bf3b7b580de78852eb22a55acddc4d
Nishant Kumar Singh <nishant220902@gmail.com>
2022-12-16 01:07:37 +0530

    converted class component to function component and added dark mode

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News'; 
 import { Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 
-export class App extends Component {
-  pageSize = 15;
-  apiKey = process.env.REACT_APP_NEWZIFY_APIKEY;
+const App = () =>  {
+  const pageSize = 15;
+  const apiKey = process.env.REACT_APP_NEWZIFY_APIKEY;
 
-  state = {
-    progress: 0,
-  };
+  const [progress, setProgress] = useState(0);
 
-  setProgress = (progress) => {
-    this.setState({progress: progress})
+  const [mode, setMode] = useState("light");
+  
+  const toggleMode = () => {
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
   }
 
-  render() {
-    document.body.style.backgroundColor="black";
+    document.body.style.backgroundColor=`${mode === "dark"? "black": "white"}`;
     return (
       <div>
         <LoadingBar
-        color='white '
-        progress={this.state.progress}
+        color={`${mode === "dark"? "white": "black"}`}
+        progress={progress}
       />
-        <Navbar/>
+        <Navbar mode={mode} toggleMode={toggleMode}/>
         <Routes>
-          <Route exact path="/" element={<News apiKey={this.apiKey}setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
-          <Route exact path="/Business" element={<News apiKey={this.apiKey}setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="in" category="business"/>}/>
-          <Route exact path="/Entertainment" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}/>
-          <Route exact path="/General" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
-          <Route exact path="/Health" element={<News apiKey={this.apiKey}setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="in" category="health"/>}/>
-          <Route exact path="/Science" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="in" category="science"/>}/>
-          <Route exact path="/Sports" element={<News apiKey={this.apiKey}setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="in" category="sports"/>}/>
-          <Route exact path="/Technology" element={<News apiKey={this.apiKey}setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="in" category="technology"/>}/>
+          <Route exact path="/" element={<News mode={mode} apiKey={apiKey}setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/>}/>
+          <Route exact path="/Business" element={<News mode={mode} apiKey={apiKey}setProgress={setProgress} key="business" pageSize={pageSize} country="in" category="business"/>}/>
+          <Route exact path="/Entertainment" element={<News mode={mode} apiKey={apiKey} setProgress={setProgress} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}/>
+          <Route exact path="/General" element={<News mode={mode} apiKey={apiKey} setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/>}/>
+          <Route exact path="/Health" element={<News mode={mode} apiKey={apiKey}setProgress={setProgress} key="health" pageSize={pageSize} country="in" category="health"/>}/>
+          <Route exact path="/Science" element={<News mode={mode} apiKey={apiKey} setProgress={setProgress} key="science" pageSize={pageSize} country="in" category="science"/>}/>
+          <Route exact path="/Sports" element={<News mode={mode} apiKey={apiKey}setProgress={setProgress} key="sports" pageSize={pageSize} country="in" category="sports"/>}/>
+          <Route exact path="/Technology" element={<News mode={mode} apiKey={apiKey}setProgress={setProgress} key="technology" pageSize={pageSize} country="in" category="technology"/>}/>
         </Routes>
       </div>
     )
   }
-}
 
 export default App
 
